96ecb32928deb80268eea3901b8e8ec08a882438
Kleber <klebernascimento@outlook.com.br>
2020-11-21 12:56:47 -0300

    Aplicando nova categoria e usando Data Anotations

--- CursoMVC/CursoMVC/Models/Produto.cs
+++ CursoMVC/CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace CursoMVC.Models
     public class Produto
     {
         public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "O campo quantidade é obrigatório.")]
+        [Range(1,10, ErrorMessage ="Valor fora da faixa. (1 à 10)")]
         public int Quantidade { get; set; }
+
+        [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
     }
